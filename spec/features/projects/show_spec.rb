3a72ed27ebf7ef3cd6c2bf509699b84292f75ecd
Daniel <danmoran.pro@gmail.com>
2020-02-07 10:39:01 -0700

    User story 1 complete

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor" do
+  describe "when I visit a project's show page" do
+    it "I see that project's name and material" do
+        challenge_1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1500)
+        project_1 = challenge_1.projects.create!(name: "Litfit", material: "Lamp Shade")
+        
+        visit project_path(project_1)
+
+        expect(page).to have_content(project_1.name)  
+        expect(page).to have_content(project_1.material)  
+        expect(page).to have_content("Apartment Furnishings")  
+    end
+end
+end
+
+# challenge_1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1500
+# contestant_1 = Contestant.create!(name: "Kentaro Kameyama", age: 25, hometown: "Denver", years_of_experience: 5 )
